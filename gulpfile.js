7b4a8c7e2c1ff0101b358ff0ea5dd6fa3531d673
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-12 17:14:49 +0300

    created 'html' task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from 'gulp'
 import { path } from './gulp/config/path.js'
 import { copy } from './gulp/tasks/copy.js'
 import { clean } from './gulp/tasks/clean.js'
+import { html } from './gulp/tasks/html.js'
 
 global.app = {
 	gulp: gulp,
@@ -10,8 +11,11 @@ global.app = {
 
 function watcher() {
 	gulp.watch(path.watch.files, copy)
+	gulp.watch(path.watch.html, html)
 }
 
-const dev = gulp.series(clean, copy, watcher)
+const mainTasks = gulp.series(copy, html)
+
+const dev = gulp.series(clean, mainTasks, watcher)
 
 gulp.task('default', dev)
