15190056f34e2845964e40c6a1f5ed351cd52516
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-12 16:08:39 +0300

    created watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 import gulp from 'gulp'
 import { path } from './gulp/config/path.js'
+import { copy } from './gulp/tasks/copy.js'
 
 global.app = {
 	gulp: gulp,
 	path: path,
 }
 
-import { copy } from './gulp/tasks/copy.js'
+function watcher() {
+	gulp.watch(path.watch.files, copy)
+}
 
-gulp.task('default', copy)
+const dev = gulp.series(copy, watcher)
+
+gulp.task('default', dev)
